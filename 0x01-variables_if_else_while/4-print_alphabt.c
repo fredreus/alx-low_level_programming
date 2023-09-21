31cbf6eadc55d9335c1359f1dcae93822c0400b0
fredreus <kbayor10@gmail.com>
2023-09-21 13:09:41 -0700

    update

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,16 +6,15 @@
 
 int main(void)
 {
-	char c;
-	c = 'a';
-
-	while
-		(c <= 'z') {
-
-			if ((c != 'q' && c != 'e') && c <= 'z')
-				putchar(c);
-			c++;
+	int i;
+	
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
 		}
+	}
 	putchar('\n');
 	return (0);
 }
